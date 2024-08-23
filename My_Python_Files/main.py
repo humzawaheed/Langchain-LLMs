fed734429f8d6fee1a24c0e6ea10a19e60bbbef5
Humza Waheed <52869334+humzawaheed@users.noreply.github.com>
2024-08-23 01:17:14 +0000

    modified:   My_Python_Files/main.py

--- My_Python_Files/main.py
+++ My_Python_Files/main.py
@@ -1,15 +1,28 @@
-from langchain_community.llms import huggingface_endpoint
+from transformers import pipeline
+sentiment_task = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest", tokenizer="cardiffnlp/twitter-roberta-base-sentiment-latest")
+result = sentiment_task("Covid cases are increasing fast!")
+dict_values = list(result[0].values()) 
+print(dict_values[0])
+
+
+# from langchain_community.llms import huggingface_endpoint
 # from huggingface_hub import hf_api
 # import os
 
 # os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 # llm = huggingface_hub.HuggingFaceHub(repo_id="finiteautomata/bertweet-base-sentiment-analysis",model_kwargs={"temperature":"0.5","max_length":"100"})
-llm = huggingface_endpoint.HuggingFaceEndpoint(huggingfacehub_api_token="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", repo_id="finiteautomata/bertweet-base-sentiment-analysis")
-prompt = "How can I code in python?"
-response = llm(prompt=prompt)
+# llm = huggingface_endpoint.HuggingFaceEndpoint(huggingfacehub_api_token="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", repo_id="finiteautomata/bertweet-base-sentiment-analysis")
 
-print(response)
+# input = llm("I love my dog!")
+
+# response = llm(prompt=input)
+
+# first_result = response[0]
+# label = first_result["label"]
+# score = first_result["score"]
+
+# print(f"Label: {label}, Score: {score}")
 
 
 
